42b418acd0167a6ff36dba2f2b9e00734fc0f480
giovannahernandes <giovanna.hernandes@escola.pr.gov.br>
2026-08-06 10:42:52 -0300

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+//Botões
+
+const bntContraste = document.getElementById("bnt-contraste");
+const bntAumentar = document.getElementById("bnt-aumentar-texto");
+const bntDiminuir = document.getElementById("bnt-diminuir-texo");
+
+//Mudar tamanho da fonte
+
+let tamanhoAtualFonte = 100;
+
+//Alto contraste
+
+bntContraste.addEventListener("click", ()=>{
+document.body.classList
+})
